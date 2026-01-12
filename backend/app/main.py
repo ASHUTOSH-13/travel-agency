97e4c1499860561d7f81c11ed28d93ba22f71d3f
Your Name <your.email@example.com>
2026-01-12 10:19:26 +0530

    added vercel domain to avoid cors error

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,8 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://lets-go-buddy-frontend.onrender.com",
-        "https://travel-agency-uq8b.onrender.com"
+        "https://travel-agency-uq8b.onrender.com",
+        "https://letsgobuddytravels.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
