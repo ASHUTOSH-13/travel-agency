03ccfbada1b3bab49298102c24e5f0c263072220
Your Name <your.email@example.com>
2026-01-14 21:20:45 +0530

    sitemap increased

--- frontend/src/app/sitemap.ts
+++ frontend/src/app/sitemap.ts
@@ -22,5 +22,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "monthly",
       priority: 0.6,
     },
+    {
+      url: `${baseUrl}/tours/shimla-manali/shimla-manali-delight`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
   ];
 }
